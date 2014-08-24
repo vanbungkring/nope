a36f2eff9cf4482847a48de240a4e9bf5e524c58
Arie Prasetyo <vanbungkring@Aries-MacBook-Pro.local>
2014-08-24 14:27:24 +0700

    init project

---
+++ homepage.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Template Name: Homepage
+*/
+
+get_header(); ?>
+
+<div id="primary" class="content-area col-md-12 homepage reset-size">
+	<main id="main" class="site-main" role="main">
+		<div class="video-first reset-size"></div>
+		<div class="row top-grid">
+			<h2>People Inspired by Us</h2>
+			<div class="col-md-3 grid"><img  align="middle" class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/images/spot1.png" alt="Detik Hot">
+				<span> Social Justice</span>
+			</div>
+			<div class="col-md-3 grid"><img  align="middle" class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/images/spot2.png" alt="Detik Hot">
+				<span> Social Justice</span>
+			</div>
+			<div class="col-md-3 grid"><img  align="middle" class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/images/spot3.png" alt="Detik Hot">
+				<span> Social Justice</span>
+			</div>
+		</div>
+		<div class="row top-grid">
+			<div class="col-md-12 cta-girls">
+				<h2 class="centering">Help us to get more girls</h2>
+			</div>
+		</div>
+		<div class="row top-grid">
+			<h2>People wanted to know...</h2>
+			<div class="col-md-4 grid-big">a</div>
+			<div class="col-md-4 grid-big">a</div>
+			<div class="col-md-4 grid-big">a</div>
+		</div>
+		<div class="row top-grid homepage-baseline">
+			<div class="col-md-1"><span>As Seen on</span></div>
+			<div class="col-md-2 grid-bottom"><a href="hot.detik.com/read/2013/08/25/103946/2339723/230/krisdayanti-dukung-house-of-perempuan--hope-"><img  align="middle" class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/images/media/logo_hot.png" alt="Detik Hot"></a></div>
+			<div class="col-md-2 grid-bottom"><a href="www.tempo.co/read/news/2013/08/25/219507218/Krisdayanti-Peduli-Nasib-Kaum-Hawa-Indonesia"><img  align="middle" class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/images/media/tempologokonten.png" alt="Tempo"></a></div>
+			<div class="col-md-2 grid-bottom"><a href="http://www.tribunnews.com/images/seleb/view/751781/krisdayanti-di-acara-peluncuran-house-of-perempuan#.U_WbfrySzwE"><img  align="middle" class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/images/media/tribunimages.png" alt="TribunNews"></a></div>
+			<div class="col-md-2 grid-bottom"><a href="http://travel.kapanlagi.com/events/lifestyle/874-house-of-perempuan-waktunya-perempuan-memiliki-harapan-baru.html"><img  align="middle" class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/images/media/logokl.png" alt="Kapanlagi"></a></div>
+			<div class="col-md-2 grid-bottom"><a href="http://www.vemale.com/hot-event/32519-harapan-baru-perempuan-dengan-hope-house-of-perempuan.html"><img  align="middle" class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/images/media/vemale.png" alt="Vemale"></a></div>
+			<div class="col-md-2 grid-bottom"><a href="http://m.koran-sindo.com/node/333951"><img  align="middle" class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/images/media/sindo.png" alt="Sindo"></a></div>
+
+		</div>
+		
+
+</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
